03f2804ab2e044ef38a1d2c46aa854d21b1a5c27
camilamiwa <miwa.ivano@gmail.com>
2022-03-07 16:29:18 -0300

    aula 9 loaine angular

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagemMain = 'https://viagemeturismo.abril.com.br/wp-content/uploads/2016/10/01241975-4f8a5b1f0f.jpeg?quality=70&strip=info&w=920?quality=70&strip=info&w=636';
+  urlImagemAerea = 'https://upload.wikimedia.org/wikipedia/commons/f/fe/Panorama_de_Macei%C3%B3.jpg';
+  urlImagemFrontal = 'https://s3q7j5f5.stackpathcdn.com/wp-content/uploads/2021/07/maceio-cidade-brasil.jpg';
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
